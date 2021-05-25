c5f5ee2d1d2d1a8b81d5ca3e238c5c48bfbc626d
patrick <patrick@adaptilab.com>
2021-05-25 13:12:06 -0700

    Add some UI tests.

--- src/dashboard_test.js
+++ src/dashboard_test.js
@@ -1,7 +1,25 @@
+
 const testFunctions = [
   // Test if chart dependencies exist
   function test1() {
     return (Chart !== undefined);
+  },
+  // Test if main_stats exists
+  function test2() {
+    return document.getElementById('main_stats') !== null;
+  },
+  function test3() {
+    return document.getElementById('display') !== null;
+  },
+  function test4() {
+    return document.getElementById('left_container') !== null;
+  },
+  function test5() {
+    return document.getElementById('right_container') !== null;
+  },
+  function test6() {
+    const chart = document.getElementById('polarChart');
+    return chart.tagName === 'CANVAS';
   },
 ];
 
@@ -9,23 +27,30 @@ const testFunctions = [
 // prints one line for each test, plus a line detailing how many tests were run
 // and how many passed.
 // If a test fails with an exception, print the exception on a new line.
-numTestsPassed = 0;
-for (let i = 0; i < testFunctions.length; i++) {
-  let message = "";
-  try {
-    if (testFunctions[i]()) {
-      numTestsPassed++;
-      message = `Test ${i} passed`;
-    } else {
-      message = `Test ${i} failed`;
+function runTests() {
+  numTestsPassed = 0;
+  for (let i = 0; i < testFunctions.length; i++) {
+    let message = "";
+    try {
+      if (testFunctions[i]()) {
+        numTestsPassed++;
+        message = `Test ${i+1} passed`;
+      } else {
+        message = `Test ${i+1} failed`;
+      }
+    } catch(error) {
+      message = `Test ${i+1} failed with exception:\n${error}`;
     }
-  } catch(error) {
-    message = `Test ${i} failed with exception:\n${error}`;
+    console.log(message);
   }
-  console.log(message);
+  console.log(`${numTestsPassed}/${testFunctions.length} tests passed`);
+  
+  if (numTestsPassed < testFunctions.length) {
+    alert("One or more tests did not pass. See the console for details.")
+  }
 }
-console.log(`${numTestsPassed}/${testFunctions.length} tests passed`);
 
-if (numTestsPassed < testFunctions.length) {
-  alert("One or more tests did not pass. See the console for details.")
-}
+// const body = document.getElementsByTagName("body")[0];
+// body.addEventListener("load", runTests, false);
+
+runTests();
